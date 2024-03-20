0ed81a3c8f6b6d856e318cb0d1cd11ad18f9f825
Drulk <156405940+Drulk@users.noreply.github.com>
2024-03-20 21:39:21 +0300

    Задания от 05.03.2024

---
+++ задание 2.2.py
@@ -0,0 +1,11 @@
+#coding utf-8
+#задача 2
+r = float(input("введите r = "))
+a = float(input("введите a = "))
+p = 3.14
+s=a**2
+b=p*r**2
+if b<s:
+    print("впишется")
+else:
+    print("не впишется")
