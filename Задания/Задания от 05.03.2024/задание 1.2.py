0f3c2fc9680fc70e8db34d6e9ab960829395ffa5
Drulk <156405940+Drulk@users.noreply.github.com>
2024-03-22 15:37:53 +0300

    задания

---
+++ Задания/Задания от 05.03.2024/задание 1.2.py
@@ -0,0 +1,6 @@
+#coding utf-8
+#задание 1
+x = float(input("введите x = "))
+y = float(input("введите y = "))
+print("максимум = ", max(x,y))
+print("минимум = ", min(x,y))
---
+++ Задания/Задания от 05.03.2024/задание 10.2.py
@@ -0,0 +1,9 @@
+#coding utf-8
+#задание 10
+x = int(input("введите x = "))
+a = int(input("введите a = "))
+if x>0:
+    y=a/x
+else:
+    y=x/a
+print("y = ", y)
