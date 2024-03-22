b3c11a1881e109119144511e1eb1abad1b2e62cc
Drulk <156405940+Drulk@users.noreply.github.com>
2024-03-22 15:40:44 +0300

    Задачи

---
+++ Задания/Задачи от *.01.2024/work 1,5 .py
@@ -0,0 +1,9 @@
+my_list = [True, 786, 3.14, 'text', 70.2]
+second_list = [123, 'text']
+
+print (my_list) #Выводит весь список
+print (my_list[0]) #Выводит первый элемент списка
+print (my_list[1:3]) #Выводит со второго по третий
+print (my_list[2:]) #Выводит начиная с третьего
+print (second_list * 2) #Выводит удвоенный список
+print (my_list + second_list) #Объединяет два списка
