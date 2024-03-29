62edb71b8afd8be5234f1384d245cd91adff1c65
Drulk <156405940+Drulk@users.noreply.github.com>
2024-03-29 23:18:27 +0300

    задание 4.4

--- Задания/задания от 27.03.2024/задание 4.4.py
+++ Задания/задания от 27.03.2024/задание 4.4.py
@@ -15,7 +15,7 @@ if 1>X or X>100:
 Y = int(input("Введите ширину дверного проёмя = "))
 if 1>Y or Y>100:
     print("eror")
-if ((X>A) and (Y>C)) or ((X>C) and (Y>B)) or ((X>B) and (Y>A)) or ((X>A) and (Y>B)) or ((X>B) or (Y>C)):
+if ((X>A) and (Y>C)) or ((X>C) and (Y>B)) or ((X>B) and (Y>A)) or ((X>A) and (Y>B)) or ((X>B) or (Y>C)) or ((X>C) and (Y>A)):
     print("YES")
 else:
     print("NO")
