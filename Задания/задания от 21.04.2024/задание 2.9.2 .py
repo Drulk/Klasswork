db13929b31cd6fa30a9ba1881f132cc7a3b77d50
Drulk <156405940+Drulk@users.noreply.github.com>
2024-05-21 14:16:32 +0300

    задача 2

---
+++ Задания/задания от 21.04.2024/задание 2.9.2 .py
@@ -0,0 +1,8 @@
+#coding utf-8
+# задание 2
+A=int(input("введите число:"))
+B=int(input("введите число:"))
+i=A+1
+while i<B:
+    print(i)
+    i+=1
---
+++ Задания/задания от 21.04.2024/задание 4.9.2 .py
@@ -0,0 +1,7 @@
+#coding utf-8
+# задание 4
+N=int(input("введите число:"))
+K=N
+while 3*K>N:
+    K-=1
+print(K+1)
